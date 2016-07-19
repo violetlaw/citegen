775a2638b460bf2178c3c10c0797413f95533863
Nathan Naze <nanaze@gmail.com>
2016-07-19 00:10:15 -0700

    Fix CitationGeneratorTest to better gather files Split golden tests into separate dirs Make an AutoValue struct to contain test cases.

--- src/test/java/com/github/violetlaw/citegen/CitationGeneratorTest.java
+++ src/test/java/com/github/violetlaw/citegen/CitationGeneratorTest.java
@@ -1,23 +1,48 @@
 package com.github.violetlaw.citegen;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
-
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.Lists;
 
 public class CitationGeneratorTest {
 	
 	// TODO(nnaze): Implement parameterized tests at this point.
 
+	private static Path GOLDEN_TESTS = Paths.get("src/test/resources/goldentests");
+	private static Path INPUT = GOLDEN_TESTS.resolve("input");
+	private static Path OUTPUT = GOLDEN_TESTS.resolve("output");
 	
-  private static Path getInputOutputPath() {
-    return getResourcesPath().resolve("inputoutput");
-  }
+	@AutoValue
+	static abstract class GoldenTestCase {
+	  static GoldenTestCase create(File input, File output) {
+	    return new AutoValue_TestCase(input, output);
+	  }
 
-  private static Path getResourcesPath() {
-    return getRootPath().resolve("src/test/resources");
-  }
+	  abstract File input();
+	  abstract File output();
+	}
 
-  private static Path getRootPath() {
-    return FileSystems.getDefault().getPath(".");
-  }
+	
+	private static List<GoldenTestCase> getTestCases() throws IOException {
+		List<GoldenTestCase> testCases = Lists.newLinkedList();
+		DirectoryStream<Path> inputPaths = Files.newDirectoryStream(INPUT, "*.json");
+		for (Path inputPath : inputPaths) {
+			Path outputPath = OUTPUT.resolve(inputPath.getFileName());
+
+			testCases.add(
+					GoldenTestCase.create(inputPath.toFile(), outputPath.toFile()));
+					
+		}
+		
+		return testCases;
+	}
 }
