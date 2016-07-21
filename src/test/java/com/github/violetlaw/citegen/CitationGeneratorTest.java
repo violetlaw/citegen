569c7a5f896a22870c5ec107571a40f05f5deac9
Nathan Naze <nanaze@gmail.com>
2016-07-20 23:21:13 -0700

    update test names in preparation for Parameterized runner

--- src/test/java/com/github/violetlaw/citegen/CitationGeneratorTest.java
+++ src/test/java/com/github/violetlaw/citegen/CitationGeneratorTest.java
@@ -12,33 +12,41 @@ import java.util.List;
 
 import com.google.auto.value.AutoValue;
 import com.google.common.collect.Lists;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 public class CitationGeneratorTest {
 
   // TODO(nnaze): Implement parameterized tests at this point.
 
   private static Path GOLDEN_TESTS = Paths.get("src/test/resources/goldentests");
+
   private static Path INPUT = GOLDEN_TESTS.resolve("input");
   private static Path OUTPUT = GOLDEN_TESTS.resolve("output");
 
-  @AutoValue
-  static abstract class GoldenTestCase {
-    static GoldenTestCase create(File input, File output) {
-      return new AutoValue_TestCase(input, output);
-    }
-
-    abstract File input();
-
-    abstract File output();
+ @Parameters
+  public static Iterable<GoldenTestData> data() throws IOException {
+    return getTestCases();
   }
 
-  private static List<GoldenTestCase> getTestCases() throws IOException {
-    List<GoldenTestCase> testCases = Lists.newLinkedList();
+  @AutoValue
+  public static abstract class GoldenTestData {
+    static GoldenTestData create(Path inputPath, Path outputPath) {
+      return new AutoValue_CitationGeneratorTest_GoldenTestData(inputPath, outputPath);
+    }
+
+    abstract Path inputPath();
+
+    abstract Path outputPath();
+  }
+
+  private static List<GoldenTestData> getTestCases() throws IOException {
+    List<GoldenTestData> testCases = Lists.newLinkedList();
     DirectoryStream<Path> inputPaths = Files.newDirectoryStream(INPUT, "*.json");
     for (Path inputPath : inputPaths) {
       Path outputPath = OUTPUT.resolve(inputPath.getFileName());
 
-      testCases.add(GoldenTestCase.create(inputPath.toFile(), outputPath.toFile()));
+      testCases.add(GoldenTestData.create(inputPath, outputPath));
     }
 
     return testCases;
