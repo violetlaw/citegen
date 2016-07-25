e1ad6658fe12730485105757c20f9befa9cd08c1
Nathan Naze <nanaze@gmail.com>
2016-07-24 20:39:21 -0700

    add logic to generate TextBlock for authors

--- src/main/java/com/github/violetlaw/citegen/CitationGenerator.java
+++ src/main/java/com/github/violetlaw/citegen/CitationGenerator.java
@@ -1,12 +1,62 @@
 package com.github.violetlaw.citegen;
 
+import java.util.List;
+
 import com.github.violetlaw.citegen.Citation.CitationRequest;
 import com.github.violetlaw.citegen.Text.TextBlock;
+import com.github.violetlaw.citegen.Text.TextBlock.Builder;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
+import javax.annotation.Nullable;
 
 public class CitationGenerator {
 
   public TextBlock handleRequest(CitationRequest request) {
+
     // TODO(nnaze): Implement.
-    return TextBlock.newBuilder().build();
+    TextBlock.Builder builder = TextBlock.newBuilder();
+
+    TextBlock authorsBlock = createAuthorsTextBlock(request.getAuthorsList());
+    if (authorsBlock != null) {
+      builder.addChildren(authorsBlock);
+    }
+
+    return builder.build();
+  }
+
+  @Nullable
+  private static TextBlock createAuthorsTextBlock(List<String> authors) {
+
+    String authorsText = createAuthorsText(authors);
+    if (authorsText == null) {
+      return null;
+    }
+
+    TextBlock.Builder builder = TextBlock.newBuilder();
+    builder.setText(createAuthorsText(authors));
+    return builder.build();
+  }
+
+  @VisibleForTesting
+  static String createAuthorsText(List<String> authors) {
+    // With one author, we just list the author.
+    if (authors.size() == 1) {
+      return Iterables.getOnlyElement(authors);
+    }
+
+    // For two authors, join with ampersand.
+    if (authors.size() == 2) {
+      Joiner joiner = Joiner.on(" & ");
+      return joiner.join(authors);
+    }
+
+    // > 2, use "et al."
+    if (authors.size() > 2) {
+      String firstAuthor = authors.get(0);
+      return firstAuthor + " et al.";
+    }
+
+    return null;
   }
 }
--- src/test/java/com/github/violetlaw/citegen/CitationGeneratorTest.java
+++ src/test/java/com/github/violetlaw/citegen/CitationGeneratorTest.java
@@ -1,8 +1,35 @@
 package com.github.violetlaw.citegen;
 
 import org.junit.Test;
+import static com.google.common.truth.Truth.assertThat;
+import com.google.common.collect.ImmutableList;
 
 public class CitationGeneratorTest {
+
   @Test
-  public void foo() {}
+  public void zeroAuthorsGivesNull() {
+    assertThat(CitationGenerator.createAuthorsText(ImmutableList.<String>of())).isNull();
+  }
+
+  @Test
+  public void oneAuthor() {
+    assertThat(CitationGenerator.createAuthorsText(ImmutableList.of("Alexander Hamilton")))
+        .isEqualTo("Alexander Hamilton");
+  }
+
+  @Test
+  public void twoAuthors() {
+    assertThat(
+            CitationGenerator.createAuthorsText(
+                ImmutableList.of("Alexander Hamilton", "James Madison")))
+        .isEqualTo("Alexander Hamilton & James Madison");
+  }
+
+  @Test
+  public void moreThanTwoAuthors() {
+    assertThat(
+            CitationGenerator.createAuthorsText(
+                ImmutableList.of("Alexander Hamilton", "James Madison", "John Jay")))
+        .isEqualTo("Alexander Hamilton et al.");
+  }
 }
