d266e1afd3a5a0bdb186fb63b2e50bc5db706ab4
Nathan Naze <nanaze@gmail.com>
2016-07-31 10:35:01 -0700

    Add AutoValue to hold tag metadata, add methods to render both tag name and attributes, put test on it before usage.

--- src/main/java/com/github/violetlaw/citegen/HtmlTextRenderer.java
+++ src/main/java/com/github/violetlaw/citegen/HtmlTextRenderer.java
@@ -3,11 +3,16 @@ package com.github.violetlaw.citegen;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Queue;
 
 import com.github.violetlaw.citegen.Text.Style;
 import com.github.violetlaw.citegen.Text.TextBlock;
+import com.google.auto.value.AutoValue;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
+import com.google.common.html.HtmlEscapers;
 
 public class HtmlTextRenderer {
 
@@ -15,9 +20,9 @@ public class HtmlTextRenderer {
     List<Style> styles = textBlock.getStyleList();
 
     // Write opening tags, if any.
-    Queue<String> openTags = Lists.newLinkedList();
+    Queue<Tag> openTags = Lists.newLinkedList();
     for (Style style : styles) {
-      String tag = getTagForStyle(style);
+      Tag tag = getTagForStyle(style);
       writeOpeningTag(tag, writer);
 
       // Remember the tag we wrote so we can close it later.
@@ -31,23 +36,50 @@ public class HtmlTextRenderer {
 
     // close the tags we've open
     while (!openTags.isEmpty()) {
-      String tag = openTags.remove();
+      Tag tag = openTags.remove();
       writeClosingTag(tag, writer);
     }
   }
 
-  private void writeOpeningTag(String tag, Writer writer) throws IOException {
-    writer.write("<" + tag + ">");
+  @VisibleForTesting
+  static void writeOpeningTag(Tag tag, Writer writer) throws IOException {
+    writer.write("<");
+    writer.write(tag.tagName());
+
+    for (Entry<String, String> attribute : tag.attributes().entrySet()) {
+      writer.write(" ");
+
+      String keyValue =
+          String.format(
+              "%s=\"%s\"",
+              attribute.getKey(),
+              HtmlEscapers.htmlEscaper().escape(attribute.getValue()));
+
+      writer.write(keyValue);
+    }
+
+    writer.write(">");
   }
 
-  private void writeClosingTag(String tag, Writer writer) throws IOException {
-    writer.write("</" + tag + ">");
+  private static void writeClosingTag(Tag tag, Writer writer) throws IOException {
+    writer.write("</" + tag.tagName() + ">");
   }
 
-  private String getTagForStyle(Style style) {
+  @AutoValue
+  abstract static class Tag {
+    static Tag create(String tagName, ImmutableMap<String, String> attributes) {
+      return new AutoValue_HtmlTextRenderer_Tag(tagName, attributes);
+    }
+
+    abstract String tagName();
+
+    abstract ImmutableMap<String, String> attributes();
+  }
+
+  private Tag getTagForStyle(Style style) {
     switch (style.getNumber()) {
       case Style.ITALICS_VALUE:
-        return "i";
+        return Tag.create("i", ImmutableMap.of());
     }
 
     throw new IllegalArgumentException("No valid tag for " + style);
--- src/test/java/com/github/violetlaw/citegen/HtmlTextRendererTest.java
+++ src/test/java/com/github/violetlaw/citegen/HtmlTextRendererTest.java
@@ -2,30 +2,67 @@ package com.github.violetlaw.citegen;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.io.Writer;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.experimental.runners.Enclosed;
 
+import com.github.violetlaw.citegen.HtmlTextRenderer.Tag;
 import com.github.violetlaw.citegen.Text.Style;
 import com.github.violetlaw.citegen.Text.TextBlock;
-import com.github.violetlaw.citegen.Text.TextBlock.Builder;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import static com.google.common.truth.Truth.assertThat;
 
+@RunWith(Enclosed.class)
 public class HtmlTextRendererTest {
-  @Test
-  public void testTextRender() throws IOException {
-    TextBlock.Builder builder = TextBlock.newBuilder();
 
-    TextBlock blockB = TextBlock.newBuilder().setText("b").addStyle(Style.ITALICS).build();
+  public static class TagWriterTest {
+    @Test
+    public void testWriteOpeningTagNoAttributes() throws IOException {
+      Tag testTag = Tag.create("test", ImmutableMap.<String, String>of());
+      assertOpeningTag(testTag, "<test>");
+    }
 
-    builder.addAllChildren(Lists.newArrayList(createTextBlock("a"), blockB, createTextBlock("c")));
+    @Test
+    public void testWriteOpeningTagWithMultipleAttributes() throws IOException {
+      Tag testTag =
+          Tag.create(
+              "test", ImmutableMap.<String, String>of("barKey", "barValue", "bazKey", "bazValue"));
+      assertOpeningTag(testTag, "<test barKey=\"barValue\" bazKey=\"bazValue\">");
+    }
 
-    TextBlock testTextBlock = builder.build();
+    @Test
+    public void testWriteOpeningQuoteEscaping() throws IOException {
+      Tag testTag = Tag.create("foo", ImmutableMap.<String, String>of("quote", "abc \"def\" ghi"));
+      assertOpeningTag(testTag, "<foo quote=\"abc &quot;def&quot; ghi\">");
+    }
 
-    String testOutput = renderTextBlock(testTextBlock);
+    private void assertOpeningTag(Tag tag, String output) throws IOException {
+      StringWriter writer = new StringWriter();
+      HtmlTextRenderer.writeOpeningTag(tag, writer);
+      assertThat(writer.toString()).isEqualTo(output);
+    }
+  }
 
-    assertThat(testOutput).isEqualTo("a<i>b</i>c");
+  public static class RenderTest {
+
+    @Test
+    public void testTextRender() throws IOException {
+
+      TextBlock.Builder builder = TextBlock.newBuilder();
+
+      TextBlock blockB = TextBlock.newBuilder().setText("b").addStyle(Style.ITALICS).build();
+
+      builder.addAllChildren(
+          Lists.newArrayList(createTextBlock("a"), blockB, createTextBlock("c")));
+
+      TextBlock testTextBlock = builder.build();
+
+      String testOutput = renderTextBlock(testTextBlock);
+
+      assertThat(testOutput).isEqualTo("a<i>b</i>c");
+    }
   }
 
   private static String renderTextBlock(TextBlock testTextBlock) throws IOException {
